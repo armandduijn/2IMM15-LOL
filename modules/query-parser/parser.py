9fe1c81c8e8f6d9d87bfebcbf13bd27acdd01352
Geert Derks <g.a.f.derks@student.tue.nl>
2017-09-26 21:31:37 +0200

    Implemented stemming of queries

--- modules/query-parser/parser.py
+++ modules/query-parser/parser.py
@@ -1,7 +1,9 @@
 from pyparsing import Word, alphanums, Keyword, Group, Combine, Forward, Suppress, Optional, OneOrMore, oneOf, Literal, nums, ZeroOrMore
 from pprint import pprint
 from var_dump import var_dump
+from modules.stemming.porter2 import stem
 import json
+import re
 
 def Syntax():
     operatorOr = Forward()
@@ -40,11 +42,19 @@ def Syntax():
 
     return operatorOr
 
+def stemWord(matchobj):
+     # if necessary ignore keywords in the stemming, but the Porter stemmer doesn't affect them.
+     return stem(matchobj.group(0))
+
+def stemQueryString(queryString):
+    queryString = re.sub(r'([a-zA-Z]*)', stemWord, queryString)
+    return queryString
+
 if __name__ == "__main__":
     expr = Syntax()
     def test(s):
-        results = expr.parseString( s )
+        results = expr.parseString( stemQueryString(s) )
         print s+'\n->\n'+results.dump()
 
     #test( "(9 AND 3)" )
-    test( "a or b or b and (not d or (not e and f) and g)" )
+    test( "data or usage or mining and (not porter or (not retrieval and qualified) and greedy)" )
