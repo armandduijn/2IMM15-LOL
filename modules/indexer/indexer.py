d883ecab93034671c966b9238cb5b7a54b5db994
rrIrene <irene.panuju@gmail.com>
2017-09-27 17:17:28 +0200

    Implemented Get* functions in indexer
    @gafderks you might need to have a copy of papers.csv in your query
    package folder (i can't push the csv to Git because it's too big...)

--- modules/indexer/indexer.py
+++ modules/indexer/indexer.py
@@ -1,47 +1,83 @@
 from pyparsing import *
 from modules.stemming.porter2 import stem
 import csv
-import sys
 import pickle
+from sets import Set
 
-file_input = sys.argv[1]
-file_output = sys.argv[2]
+def Index(file_dataset = "papers.csv", file_dump = "output.p", id_col = 0, text_col = -1):
 
-id_col = 0
-text_col = -1
+    try:
+        arr = pickle.load(open(file_dump, "rb"))
+        collection = arr[0]
+        docs = arr[1]
+        print "Retrieved index from file " + file_dump
+    except (OSError, IOError) as e:
+        collection = {}
+        doc_nr = 0
+        docs = {}
 
-doc_nr = 0
+        with open(file_dataset, "r") as paperscsv:
+            papersreader = csv.reader(paperscsv,delimiter=',',quotechar='"')
+            for doc in papersreader:
+                # skip the header
+                if doc_nr > 0:
+                    id = doc[id_col]
+                    text = doc[text_col]
+                    docs[id] = text
+                    # extract tokens from the title and the text
+                    # TODO: improve parser grammar to also include numbers
+                    grammar = Word(alphas)
+                    token_pos = 0
+                    for token,start,end in grammar.scanString(text):
+                        # token is still in array. get the first element to get the string, and then stem this.
+                        token = stem(str(token[0]).lower())
+                        token_pos += 1
+                        doc_terms = []
+                        if collection.has_key(token):
+                            doc_dict = dict(collection[token])
+                            if doc_dict.has_key(id):
+                                doc_terms = doc_dict[id]
+                            else:
+                                collection[token][id] = doc_terms
+                        else:
+                            collection[token] = {id:doc_terms}
+                            doc_terms.append(token_pos)
+                doc_nr += 1
 
-collection = {}
+        pickle.dump([collection, docs], open(file_dump, "wb"))
 
-with open(file_input, "r") as paperscsv:
-    papersreader = csv.reader(paperscsv,delimiter=',',quotechar='"')
-    for doc in papersreader:
-        # skip the header
-        if doc_nr > 0:
-            id = doc[id_col]
-            text = doc[text_col]
-            # extract tokens from the title and the text
-            # TODO: improve parser grammar to also include numbers
-            grammar = Word(alphas)
-            token_pos = 0
-            for token,start,end in grammar.scanString(text):
-                # token is still in array. get the first element to get the string, and then stem this.
-                # TODO: improve the stemming to also normalize plural words
-                token = stem(str(token[0])).lower()
-                token_pos += 1
-                doc_terms = []
-                if collection.has_key(token):
-                    doc_dict = dict(collection[token])
-                    if doc_dict.has_key(id):
-                        doc_terms = doc_dict[id]
-                    else:
-                        collection[token][id] = doc_terms
-                else:
-                    collection[token] = {id:doc_terms}
-                doc_terms.append(token_pos)
-        doc_nr += 1
+        print "Retrieved index by building it from dataset: " + file_dataset
 
-print(collection)
+    return [collection,docs]
 
-pickle.dump(collection, open(file_output, "wb"))
+def GetWord(word):
+    index = Index()[0]
+    if (index.has_key(word)):
+        return Set(index[word].keys())
+    else:
+        return Set()
+
+def GetWordWildcard(self, word):
+    result = Set()
+    index = Index()[0]
+    for item in index.keys():
+        if word == item[0:len(word)]:
+            result = result.union(Set(index[item].keys()))
+    return result
+
+def GetQuotes(self, search_string, tmp_result):
+    result = Set()
+    docs = Index()[1]
+    for item in tmp_result:
+        if docs[item].count(search_string):
+            result.add(item)
+    return result
+
+def GetNot(self, not_set):
+    docs = Index()[1]
+    all = Set(docs.keys())
+    return all.difference(not_set)
+
+if __name__ == "__main__":
+    test = Index()
+    print test.index
