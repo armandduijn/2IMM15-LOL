b8eadca5b15a4dd9a049ca40e1550eff9a4a77ef
rrIrene <irene.panuju@gmail.com>
2017-10-15 21:10:21 +0200

    New index built from sqlite
    Also changed dump location of index to data/derived folder

--- modules/indexer/indexer.py
+++ modules/indexer/indexer.py
@@ -1,12 +1,15 @@
 from pyparsing import *
 from modules.stemming.porter2 import stem
 import csv
-import pickle
+import cPickle as pickle
 import os
 from sets import Set
 import pprint
 import math
 import operator
+import sqlite3
+from collections import defaultdict
+import functools
 
 pp = pprint.PrettyPrinter(indent=2)
 
@@ -14,53 +17,60 @@ pp = pprint.PrettyPrinter(indent=2)
 
 os.chdir(os.path.dirname(__file__))
 
-def Index(file_dataset = "papers.csv", file_dump = "output.lol", id_col = 0, text_col = -1):
+
+def Index(file_dataset = "../../data/papers.csv", file_dump = "../../data/derived/", id_col = 0, text_col = -1):
 
     try:
-        arr = pickle.load(open(file_dump, "rb"))
-        collection = arr[0]
-        idf = arr[1]
-        docs = arr[2]
+        collection = pickle.load(open(file_dump + "index.lol", "rb"))
+        idf = pickle.load(open(file_dump + "idf.lol", "rb"))
+        docs = pickle.load(open(file_dump + "doc_length.lol", "rb"))
         #print "Retrieved index from file " + file_dump
     except:
-        collection = {}
+        collection = defaultdict(functools.partial(defaultdict, list))
         doc_nr = 0
         idf = {}
         docs  = {}
 
         print "Building indexer..."
 
-        with open(file_dataset, "r") as paperscsv:
-            papersreader = csv.reader(paperscsv,delimiter=',',quotechar='"')
-            for doc in papersreader:
+        #with open(file_dataset, "r") as paperscsv:
+            #papersreader = csv.reader(paperscsv,delimiter=',')
+        with sqlite3.connect(os.getcwd() + '/../../data/database.sqlite') as database:
+            cursor = database.cursor()
+            cursor.execute('SELECT * FROM papers')
+            for doc in cursor.fetchall():
+            #for doc in papersreader:
                 # skip the header
-                if doc_nr > 0:
-                    id = doc[id_col]
-                    text = doc[text_col]
-                    # extract tokens from the title and the text
-                    # TODO: improve parser grammar to also include numbers
-                    grammar = Word(alphas)
-                    token_pos = 0
-                    print "Examining doc: " + id
-                    for token,start,end in grammar.scanString(text):
-                        # token is still in array. get the first element to get the string, and then stem this.
-                        token = stem(str(token[0]).lower())
-                        token_pos += 1
-                        if collection.has_key(token):
-                            doc_dict = dict(collection[token])
-                            if doc_dict.has_key(id):
-                                doc_dict[id].append(token_pos)
-                            else:
-                                collection[token][id] = [token_pos]
-                        else:
-                            collection[token] = {id:[token_pos]}
-                    docs[id] = token_pos
+                #if doc_nr > 0:
+                id = doc[id_col]
+                text = doc[text_col]
+                # extract tokens from the title and the text
+                token_pos = 0
+                print "Examining doc: " + str(id)
+                tokenized_text = Word(alphas).searchString(text)
+                docs[id] = len(tokenized_text)
+                for token in tokenized_text:
+                    token = stem(str(token[0]).lower())
+                    token_pos += 1
+                    collection[token][id].append(token_pos)
                 doc_nr += 1
 
-        for term in collection.keys():
-            idf[term] = math.log10((doc_nr - 1.0)/len(collection[term]))
+        print "Calculating idf..."
 
-        pickle.dump([collection, idf, docs], open(file_dump, "wb"))
+        for term in collection.keys():
+            idf[term] = math.log10(doc_nr/float(len(collection[term])))
+
+        print "Dumping index..."
+
+        pickle.dump(collection,open(file_dump + "index.lol", "wb"))
+
+        print "Dumping idf..."
+
+        pickle.dump(idf, open(file_dump + "idf.lol", "wb"))
+
+        print "Dumping doc_length..."
+
+        pickle.dump(docs, open(file_dump + "doc_length.lol", "wb"))
 
     return [collection, idf, docs]
 
@@ -87,9 +97,11 @@ def GetQuotes(search_string, tmp_result):
             result.add(item)
     return result
 
-def GetQuery(query):
+def VSMSearch(query):
     # this function returns the result based on vector space model
     # the result contains of doc ids sorted by similarity to query, and the first position of relevant token in doc
+
+    print "query: " + query
 
     # get tokens from query
     q_grammar = Word(alphas)
@@ -97,6 +109,8 @@ def GetQuery(query):
     q_length = 0
     for token, start, end in q_grammar.scanString(query):
         token = stem(str(token[0]).lower())
+        token = unicode(token, "utf-8")
+        print "stemmed token: " + token
         if q_tf.has_key(token):
             q_tf[token] += 1
         else:
@@ -112,12 +126,13 @@ def GetQuery(query):
     first_pos = {}
 
     # get the collection's tf idf with the query's tf idf
+    k = 0
     for term in q_tf.keys():
         if d_idf.has_key(term):
             q_score = q_tf[term] * d_idf[term]
             for doc in vocabulary[term]:
                 posting = vocabulary[term][doc]
-                d_score = math.log10(len(posting)) * d_idf[term] * q_score
+                d_score = len(posting) * d_idf[term] * q_score
                 if d_score_list.has_key(doc):
                     d_score_list[doc] += d_score
                 else:
@@ -136,12 +151,23 @@ def GetQuery(query):
 
     #print sorted(d_score_list.items(), key=operator.itemgetter(0))
     #print sorted_scores
-    #print "length of result: " + str(len(sorted_scores))
+    print "length of result: " + str(len(sorted_scores))
 
     # include the first position of query term as found in the posting for each doc
-    result = {}
+
+    database = sqlite3.connect(os.getcwd() + '/../../data/database.sqlite')
+    cursor = database.cursor()
+
+    result = []
+    counter = 1
     for doc, weight in sorted_scores:
-        result[doc] = first_pos[doc]
+        result.append([doc, first_pos[doc]])
+        cursor.execute("select title from papers where id=" + str(doc))
+        for row in cursor.fetchall():
+            print str(counter) + ". " + row[0] + ", score: " + str(weight)
+        counter += 1
+
+    database.close()
 
     #print result
 
@@ -158,4 +184,4 @@ if __name__ == "__main__":
     #print pp.pformat(test[0])
     #print pp.pformat(test[1])
 
-    GetQuery("data mining")
+    VSMSearch("Latent Dirichlet")
