dda9328c7380fed70f2ccc6ddec11da25c12d5dc
Armand Duijn <armand@duijn.space>
2017-10-16 19:11:44 +0200

    Clean up code

--- backend/src/module/AbstractModule.php
+++ backend/src/module/AbstractModule.php
@@ -4,30 +4,16 @@ namespace App;
 
 use Psr\Container\ContainerInterface;
 
-abstract class AbstractModule implements ContainerAwareInterface
+abstract class AbstractModule
 {
     protected $title = 'Component';
-
-    /**
-     * @var
-     */
-    private $container;
-
-    /**
-     * @param ContainerInterface|null $container
-     * @return void
-     */
-    public function setContainer(ContainerInterface $container = null): void
-    {
-        $this->container = $container;
-    }
 
     /**
      * @return ContainerInterface
      */
     protected function getContainer(): ContainerInterface
     {
-        return $this->container;
+        return Container::getContainer();
     }
 
     public function getTitle(): string
