65e36609bb8a1afdc79c6e7ee547e508e82ab2c9
Armand Duijn <armand@duijn.space>
2017-10-17 15:52:45 +0200

    Query author names from the database

--- backend/src/module/Similarity/Similarity.php
+++ backend/src/module/Similarity/Similarity.php
@@ -37,6 +37,31 @@ class Similarity extends AbstractModule implements RenderableInterface
             $results = array_map(null, $matches[1], $matches[2]);
         };
 
+        // Limit the amount of results
+        // The first result (100%) is the author himself.
+        $results = array_slice($results, 1, 5);
+
+        // Filter out irrelevant authors (below a threshold)
+        $results = array_filter($results, function ($result) {
+            $threshold = 0.5;
+
+            return ((float) $result[1]) >= $threshold;
+        });
+
+        // Generate an array of only author IDs
+        $ids = array_map(function ($result) {
+            return $result[0];
+        }, $results);
+
+        /** @var \PDO $connection */
+        $connection = $this->getContainer()->get(\PDO::class);
+        $statement = $connection->query('SELECT * FROM authors WHERE id IN (' . implode(',', $ids) . ')');
+
+        foreach ($statement->fetchAll() as $index => $data) {
+            // Assume that the results are in the correct order
+            $results[$index][] = $data['name'];
+        }
+
         return Helper::render(__DIR__ . '/view/similarity.phtml', [
             'results' => $results
         ]);
