cd7bcbb874e2a247bd56c6d637a51dfe1a4fd247
Armand Duijn <armand@duijn.space>
2017-10-18 19:59:29 +0200

    Change module loading in bootstrap

--- modules/bootstrap.py
+++ modules/bootstrap.py
@@ -4,6 +4,7 @@ import pickle
 import os
 import imp
 import sys
+import importlib
 
 def main():
     container = {}
@@ -13,20 +14,25 @@ def main():
 
     directories = filter(lambda x: os.path.isdir(x), os.listdir(os.getcwd()))
 
+    # Keep a reference
+    cwd = os.getcwd()
+
     for directory in directories:
-        directory = os.path.abspath(directory)
+        full_path = os.path.abspath(directory)
 
-        if os.path.exists(directory + "/main.py"):
-            sys.path.append(directory)
+        os.chdir(cwd)
 
-            module = imp.load_source('module.name', directory + "/main.py")
+        if os.path.exists(full_path + "/main.py"):
+            module = importlib.import_module('modules.' + directory + '.main')
 
             if hasattr(module, 'register'):
                 if hasattr(module, 'MODULE_NAME'):
-                    print("Importing '%s'..." % module.MODULE_NAME, color='green')
+                    print("Importing '%s'..." % module.MODULE_NAME, color="green")
+                else:
+                    print("Importing '%s'..." % directory, color="green")
 
                 module.register(container);
             else:
-                print("No register() function found in '%s/main.py'" % directory, color='yellow')
+                print("No register() function found in '%s/main.py'" % full_path, color="yellow")
 
     return container
