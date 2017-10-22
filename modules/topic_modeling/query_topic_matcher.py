308102f32119b9059dd7de72204b0952fe3a5497
wjwka <819365420@qq.com>
2017-10-22 21:38:37 +0200

    query_topic_matcher
    input: raw query string
    output: the most matched topic id

--- modules/topic_modeling/query_topic_matcher.py
+++ modules/topic_modeling/query_topic_matcher.py
@@ -1,28 +1,26 @@
+### This is for query--topic matching ,
+##input: query,,,,
+##output: topic id
 import gensim
 import string
 import json
 import nltk
-nltk.download('wordnet')
+# nltk.download('wordnet')
 from nltk.stem.wordnet import WordNetLemmatizer
-from gensim import corpora, models, similarities, utils
-from nltk.stem.lancaster import LancasterStemmer
-st = LancasterStemmer()
 lm = WordNetLemmatizer()
 
-query = 'lda'
-print "query: " + query
-print "searching for documents in the same topic..."
+#TODO: PHP stuff!? query send to here!!!!!
+# TODO: it should be a raw string  because I  need to lemmatize it instead of stem it
+query = 'machine learning'
+# print "query: " + query
+# print "searching for documents in the same topic..."
 query = query.split(' ')
 for word in query:
     word = lm.lemmatize(word)
 
 
-with open('./topics/topicid_docid.txt','r') as f:
-    topicid_docid = json.load(f)
-
 lda = gensim.models.LdaModel.load('./topics/lda_topic8.lda')
 dictionary = gensim.corpora.Dictionary.load('./topics/alltoken.dict')
-
 
 id2word = gensim.corpora.Dictionary()
 _ = id2word.merge_with(dictionary)
@@ -30,5 +28,10 @@ _ = id2word.merge_with(dictionary)
 query = id2word.doc2bow(query)
 a = list(sorted(lda[query], key=lambda x: -x[1]))
 most_matched_topic = a[0][0]
-print "topic" + str(most_matched_topic)
-print topicid_docid[str(most_matched_topic)]
+
+
+#####here is output####
+
+
+most_matched_topic = str(most_matched_topic)
+print most_matched_topic
