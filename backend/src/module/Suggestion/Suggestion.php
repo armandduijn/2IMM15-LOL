f67fd99d1f5e72d4983084f365cfe052475c2a7e
Armand Duijn <armand@duijn.space>
2017-10-25 14:19:47 +0200

    Trim input

--- backend/src/module/Suggestion/Suggestion.php
+++ backend/src/module/Suggestion/Suggestion.php
@@ -16,26 +16,26 @@ class Suggestion extends AbstractModule
     {
         return 'Suggestion';
     }
-    
+
     public function giveSuggestions($query) {
         // parse query
-        $pieces = explode(' ', $query);
+        $pieces = explode(' ', trim($query));
         $lastWord = array_pop($pieces);
-        
+
         // correct last word
         $correctedLastWord = Helper::runOnServer('spelling', $lastWord, false);
-        
+
         $stem = Porter::stem($correctedLastWord);
-        
+
         $db = new PDO('sqlite:'.__DIR__.'/../../../../data/derived/bigram.sqlite');
         $statement = $db->prepare('SELECT suggestion, count FROM bigram WHERE stem = :stem ORDER BY count DESC LIMIT 10');
         $statement->execute([':stem' => $stem]);
         $suggestions = [];
-        
+
         while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
             $suggestions[] = trim(join(" ", $pieces)." ".$correctedLastWord." ".$row['suggestion']);
         }
-        
+
         return $suggestions;
     }
 }
